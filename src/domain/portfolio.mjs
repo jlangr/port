b6f68b365643fe7cf3356583017bd951e6b0ce0e
Jeff Langr <jeff@langrsoft.com>
2025-07-23 21:12:54 -0600

    Revert "something not good in a previous gen of getPositionFromTransactions"
    This reverts commit 47895105a0b84253849ccb1df9672a799b4c645b.

--- src/domain/portfolio.mjs
+++ src/domain/portfolio.mjs
@@ -25,15 +25,3 @@ export const applyTransactions = transactions =>
 
 export const getTransactionsBySymbol = (transactions, symbol) =>
   transactions.filter(isSymbol(symbol))
-
-const sumShares = txns =>
-  txns.reduce((total, t) => total + t.shares, 0)
-
-const getSymbol = txns =>
-  txns[0]?.symbol
-
-export const getPositionFromTransactions = txns => ({
-  symbol: getSymbol(txns),
-  shares: sumShares(txns),
-  numberOfTransactions: txns.length
-})
